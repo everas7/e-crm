0f07459375e24de0a6676ac777323d7a2f3292fd
Enmanuel Veras <e.veras@gbh.com.do>
2020-09-25 11:48:50 -0400

    Adds button and component for employees creation

---
+++ web-app/src/app/features/employees/employees-form/employees-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employees-form',
+  templateUrl: './employees-form.component.html',
+  styleUrls: ['./employees-form.component.css']
+})
+export class EmployeesFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- web-app/src/app/shared/shared.module.ts
+++ web-app/src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from './navbar/navbar.component';
 import { TableComponent } from './table/table.component';
 import { ListViewComponent } from './list-view/list-view.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
-import { RouterModule } from '@angular/router';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   imports: [
@@ -17,18 +19,21 @@ import { RouterModule } from '@angular/router';
     MatTableModule,
     MatListModule,
     RouterModule,
+    MatButtonModule,
   ],
   declarations: [
     NavbarComponent,
     TableComponent,
     ListViewComponent,
     BreadcrumbComponent,
+    ButtonComponent,
   ],
   exports: [
     NavbarComponent,
     TableComponent,
     ListViewComponent,
     BreadcrumbComponent,
+    ButtonComponent,
   ],
 })
 export class SharedModule {}
